426a90088270ef8b2c86ca44dff938fab41c3280
Zoran Đukić <zorandjukic.os@gmail.com>
2021-07-18 12:55:15 +0200

    Napravio "O nama".
    Napravio "O nama". Treba još napraviti da se vrati na glavni izbornik i treba napisati na stranicu od GitHuba o samom projektu.

--- FitApp/src/fitnessapp/Start.java
+++ FitApp/src/fitnessapp/Start.java
@@ -1,11 +1,14 @@
 package fitnessapp;
 
+import java.awt.Desktop;
+import java.net.URI;
+import java.net.URL;
 import java.util.List;
 
 public class Start {
-	
-	private List<Hrana>hrana; 
-	private List<Aktivnosti>aktivnosti;
+
+	private List<Hrana> hrana;
+	private List<Aktivnosti> aktivnosti;
 
 	public Start() {
 
@@ -21,23 +24,49 @@ public class Start {
 		System.out.println("3. Unos aktivnosti");
 		System.out.println("4. Postavke");
 		System.out.println("5. O nama");
+		System.out.println("6. Izlazak iz aplikacije");
 		ucitajGlavnuAkciju();
 
 	}
 
 	private void ucitajGlavnuAkciju() {
-		switch() {
+		switch (Ulaz.ucitajInt("Izaberite stavku", "Niste unijeli cijeli broj", 1, 5)) {
 		case 1 -> dnevnikIzbornik();
 		case 2 -> izbornikHrane();
 		case 3 -> izbornikAktivnosti();
 		case 4 -> postavkeIzbornik();
 		case 5 -> oNamaIzbornik();
+		case 6 -> izlazIzAplikacije();
 		}
-		
+
+	}
+
+	private void izlazIzAplikacije() {
+
 	}
 
 	private void oNamaIzbornik() {
 
+		System.out.println("Program je napravio Zoran Đukić za završni rad iz tečaja Jave u Edunovi. ");
+		System.out.println("Ako želite znati više upišite da, ako se želite vratiti na glavni izbornik upišite ne.5");
+
+		switch (Ulaz.daIliNe("Želite li znati više", "Neispravan unos. Možete samo odgovoriti sa da ili ne", "da",
+				"ne")) {
+		case "da" -> ucitajLink();
+		case "ne" -> ucitajGlavnuAkciju();
+
+		}
+
+	}
+
+	private void ucitajLink() {
+		try {
+			Desktop desktop = java.awt.Desktop.getDesktop();
+			URI oURL = new URI("https://github.com/z-dukic/ZavrsniRadEdunova");
+			desktop.browse(oURL);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 
 	private void postavkeIzbornik() {
--- FitApp/src/fitnessapp/Ulaz.java
+++ FitApp/src/fitnessapp/Ulaz.java
@@ -1,5 +1,50 @@
 package fitnessapp;
 
+import java.util.Scanner;
+
 public class Ulaz {
+
+	public static Scanner scanner = new Scanner(System.in);
+
+	public static int ucitajInt(String poruka, String greska, int min, int max) {
+		int broj = 0;
+		while (true) {
+			System.out.println(poruka);
+			try {
+
+				broj = Integer.parseInt(scanner.nextLine());
+				if (broj < min || broj < max) {
+					System.out.println("Broj ne smije biti manji od: " + min + " ili veći od " + max);
+					continue;
+				}
+				break;
+
+			} catch (Exception e) {
+				System.out.println(greska);
+			}
+		}
+
+		return broj;
+	}
+
+	public static String daIliNe(String poruka, String greska, String da, String ne) {
+		String izjava = "";
+
+		while (true) {
+			try {
+				izjava = scanner.nextLine();
+				if (izjava == "da" || izjava == "ne") {
+					System.out.println("Možete odgovoriti samo sa da ili ne");
+					continue;
+				}
+				break;
+
+			} catch (Exception e) {
+				System.out.println(greska);
+			}
+		}
+
+		return izjava;
+	}
 
 }
